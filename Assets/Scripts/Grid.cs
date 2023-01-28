54b623ab36d757c2c21f576ed19b8ec0c933ab6e
Jake <jakeedwards238@gmail.com>
2023-01-28 18:12:23 +0000

    Added grid functionality to build objects. Objects cannot be built where an object already is
    Signed-off-by: Jake <jakeedwards238@gmail.com>

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -2,7 +2,7 @@ using Mono.Cecil.Cil;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SocialPlatforms.GameCenter;
 
 public class GridCell
 {
@@ -11,6 +11,7 @@ public class GridCell
         this.Values = new Dictionary<object, object>();
         this.Values.Add("buildable", true);
         this.Values.Add("structure", null);
+        this.Values.Add("center", new Vector3(0, 0, 0));
     }
 
     public Dictionary<object, object> Values
@@ -18,17 +19,11 @@ public class GridCell
         get;
         set;
     }
-    
-    public void Awake()
-    {
-        
-    }
 
 }
 
 public class Grid
 {
-
     private int width;
     private int height;
     private float cellSize;
@@ -39,6 +34,7 @@ public class Grid
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
+        
 
         gridArray = new GridCell[width, height];
 
@@ -49,11 +45,23 @@ public class Grid
                 gridArray[x, y] = new GridCell();
                 Debug.DrawLine(GetCellWorldPosition(x, y), GetCellWorldPosition(x, y + 1), Color.white, 100f);
                 Debug.DrawLine(GetCellWorldPosition(x, y), GetCellWorldPosition(x + 1, y), Color.white, 100f);
-                gridArray[x, y].Values.Add("centerPosition", new Vector2(x, y) * cellSize - new Vector2(cellSize/2, cellSize/2));
-                Debug.Log(gridArray[x, y].Values["buildable"]);
+                gridArray[x, y].Values["center"] = new Vector3(x, y) * cellSize - new Vector3(cellSize/2, cellSize/2);
             }
         }
     }
+
+    public void Build(int x, int y, Transform obj)
+    {
+        if ((bool)gridArray[x, y].Values["buildable"] && gridArray[x, y].Values["structure"] == null) {
+            Transform instantiatedObj = GameObject.Instantiate(obj, (Vector3)gridArray[x, y].Values["center"], Quaternion.identity);
+            gridArray[x, y].Values["structure"] = instantiatedObj;
+        } else
+        {
+            Debug.Log("Unable to build " + obj + " at position " + x + " " + y + " as there is already a structure or this is not buildable");
+        }
+
+    }
+        
 
     private Vector2 GetCellWorldPosition(int x, int y)
     {
--- Assets/Scripts/GridInit.cs
+++ Assets/Scripts/GridInit.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class GridInit : MonoBehaviour
 {
+
+    public Transform tree;
     private void Start()
     {
         Grid grid = new Grid(9, 9, 10f);
+        grid.Build(2, 2, tree);
+        grid.Build(2, 2, tree);
+        grid.Build(5, 4, tree);
     }
 }
