ba437768075bc8ab8c3eb564a1badcb279d10bab
rosejake14 <jake.c.rose03@gmail.com>
2023-02-19 23:20:35 +0000

    Player Animations + Attack
    Signed-off-by: rosejake14 <jake.c.rose03@gmail.com>

--- Assets/PlayerMovement/PlayerScripts/S_Pl_Abilities.cs
+++ Assets/PlayerMovement/PlayerScripts/S_Pl_Abilities.cs
@@ -3,31 +3,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.EventSystems.EventTrigger;
+using static UnityEngine.Rendering.DebugUI;
 
 public class S_Pl_Abilities : MonoBehaviour
 {
     private bool flag = false;
     private GameObject enemy1;
     private int CameraZoomValue = 40;
-
+    Animator animator;
     public GameObject MovementScript;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        animator= GetComponent<Animator>();
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    
+
+    void Ability1()
     {
-        if (Input.GetButtonDown("Ability1"))
+        Debug.Log("MainAttack");
+        animator.Play("Attack");
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
         {
-            Debug.Log("MainAttack");
-            
+            Ability1();
+            int i;
+            for (i = 100; i > 0; i--)
+            {
+                Debug.Log(i);
+            }
+            animator.Play("Idle");
+
         }
         else if (Input.GetButtonDown("Ability2"))
         {
+            animator.Play("Idle");
             Debug.Log("SideAttack");
         }
         else if (Input.GetButtonDown("Camera"))
