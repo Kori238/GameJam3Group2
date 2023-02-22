50c4130907c8fd41f64af2bc67f2ccd611bf4d9f
Jake <jakeedwards238@gmail.com>
2023-02-22 21:00:46 +0000

    removed scriptable objects, they're totally useless. Walls are now yellow when destroyed. Structures can be damaged and healed by script
    Signed-off-by: Jake <jakeedwards238@gmail.com>

---
+++ Assets/Scripts/StructureScripts/Structure.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Structure : MonoBehaviour
+{
+
+    public float health;
+    public float maxHealth;
+    public bool damageable;
+    public bool destroyed;
+
+    public virtual void Damaged(float amount)
+    {
+        if (health - amount < 0)
+        {
+            health = 0;
+        } else
+        {
+            health -= amount;
+        }
+        
+        if (health <= 0)
+        {
+            Destroyed();
+        }
+    }
+
+    public virtual void Destroyed()
+    {
+        destroyed = true;
+        Debug.Log(gameObject.name + " has been Destroyed!");
+    }
+
+    public virtual void Demolished()
+    {
+        Destroy(gameObject);
+    }
+
+    public virtual void SetHealth(float amount, bool fullyHeal = false)
+    {
+        if (amount > maxHealth || fullyHeal) {
+            health = maxHealth;
+        } else if (amount < 0){
+            health = amount;
+        } else {
+            health = 0;
+        }
+        if (destroyed && health > 0)
+        {
+            destroyed = false;
+        }
+    }
+
+    public virtual void Healed(float amount)
+    {
+        if (health + amount > maxHealth)
+        {
+            health = maxHealth;
+        } else {
+            health += amount;
+        }
+        if (destroyed && health > 0)
+        {
+            destroyed = false;
+        }
+    }
+}
