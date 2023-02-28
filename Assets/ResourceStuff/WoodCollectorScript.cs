695fb8db649c1fcec18b0541bc4c61e426f40c89
Jake <jakeedwards238@gmail.com>
2023-02-28 15:24:38 +0000

    Revert "Pop up menu stuff"
    This reverts commit df7fe1b96aaa0bd93d853478ba7edae9161d3bdf.

---
+++ Assets/ResourceStuff/WoodCollectorScript.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodCollectorScript : Interactable
+{
+
+
+    int CollectionAmount= 50;
+
+    int CurrentWoodCoolDown = 0;
+    int MaxWoodCoolDown = 500;
+
+    int currentHealth =50;
+    int maxHealth = 100;
+
+    int MAssigned = 3 ; // the ammount of minions assigned to the buiding 
+    int MaxMAssigned = 5;
+
+    int CurrentBuildingLevel=0;// building level 1
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CurrentWoodCoolDown >= MaxWoodCoolDown)
+        {
+            Init.Instance.resourceManager.AddWood((MAssigned/MaxMAssigned)*CollectionAmount);
+            //resourceManager.AddWood(50);
+            print("Wood Collector added "+ ((MAssigned / MaxMAssigned) * CollectionAmount) + " wood");
+            CurrentWoodCoolDown = 0;
+        }
+        if(currentHealth<=0) 
+        { 
+        Destroy(gameObject);
+        }
+    }
+    private void FixedUpdate()
+    {
+        CurrentWoodCoolDown +=  1;
+
+    }
+    public override void Interact()
+    {
+        if(Init.Instance.resourceManager.GetWood()>= 50)
+        {
+            currentHealth = maxHealth;
+            Debug.Log("building repaired");
+        }
+        else
+        {
+            Debug.Log("failed to repair");
+        }
+
+    }
+    private void upgrade()
+    {
+        switch (CurrentBuildingLevel)
+        {
+            case 0:// upgades to level 2
+                {
+                    MaxMAssigned = 10;
+                    break;
+                }
+               
+        }
+    }
+}
