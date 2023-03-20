0e106c61a5589cdbe0cfe0eed6336474c6ce7d84
rosejake14 <jake.c.rose03@gmail.com>
2023-03-20 12:40:10 +0000

    Clock rotation matches in-game time
    Signed-off-by: rosejake14 <jake.c.rose03@gmail.com>

--- Assets/TimeController.cs
+++ Assets/TimeController.cs
@@ -5,12 +5,11 @@ public class TimeController : MonoBehaviour
 {
     public float secoundsTime;
     public float minutesTime;
-    public float hourTime = 6;
+    public float hourTime = 12;
     public GameObject clock;
     public TMP_Text timer;
     float clockRotation = 0;
     public int timeMultiplyer = 10;
-
     // Start is called before the first frame update
     private void Start()
     {
@@ -19,7 +18,7 @@ public class TimeController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        minutesTime += Time.deltaTime * timeMultiplyer;
+        minutesTime += Time.deltaTime * timeMultiplyer * 4;
         clockRotation += Time.deltaTime * timeMultiplyer;
 
         if(minutesTime <= 9)
@@ -31,7 +30,7 @@ public class TimeController : MonoBehaviour
             timer.text = hourTime + ": " + (int)minutesTime;
         }
         
-        var rotateClock = new Vector3(0, 0, (float)(1.6*minutesTime));
+        var rotateClock = new Vector3(0, 0, (float)(clockRotation));
         clock.transform.eulerAngles = rotateClock;
         if (minutesTime >= 60)
         {
