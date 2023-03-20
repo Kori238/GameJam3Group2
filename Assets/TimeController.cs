2aa44cf65c43ce0da0a7aa8be56f502e798ba866
rosejake14 <jake.c.rose03@gmail.com>
2023-03-20 12:29:42 +0000

    clock debug + improvements Signed-off-by: rosejake14 <jake.c.rose03@gmail.com>

--- Assets/TimeController.cs
+++ Assets/TimeController.cs
@@ -8,7 +8,8 @@ public class TimeController : MonoBehaviour
     public float hourTime = 6;
     public GameObject clock;
     public TMP_Text timer;
-
+    float clockRotation = 0;
+    public int timeMultiplyer = 10;
 
     // Start is called before the first frame update
     private void Start()
@@ -18,9 +19,19 @@ public class TimeController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        minutesTime += Time.deltaTime * 2;
-        timer.text = hourTime + ":" + (int)minutesTime;
-        var rotateClock = new Vector3(0, 0, minutesTime);
+        minutesTime += Time.deltaTime * timeMultiplyer;
+        clockRotation += Time.deltaTime * timeMultiplyer;
+
+        if(minutesTime <= 9)
+        {
+            timer.text = hourTime + ": 0" + (int)minutesTime;
+        }
+        else
+        {
+            timer.text = hourTime + ": " + (int)minutesTime;
+        }
+        
+        var rotateClock = new Vector3(0, 0, (float)(1.6*minutesTime));
         clock.transform.eulerAngles = rotateClock;
         if (minutesTime >= 60)
         {
